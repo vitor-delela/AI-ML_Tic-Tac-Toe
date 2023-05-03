0ea1d5e5b90c84d3af2d6e4607b2b7357db207a9
Vitor Delela <vitordelela@MacBook-Air-vd.local>
2023-05-03 12:10:29 -0300

    frontend

--- TicTacToeKnn.py
+++ TicTacToeKnn.py
@@ -16,32 +16,80 @@ for linha in data:
     dadosClassificaco.append(','.join(valores[9:]))
 
 # Define o dicionário de mapeamento
-mapa = {'x': 1, 'o': -1, 'b': 0}
+mapa = {'x': 1, 'o': -1, 'b': 0, 'X': 1, 'O': -1, 'B': 0}
 
 valores_numeros = []
 for entrada in dadosTabuleiro:
-    # Separa os valores da entrada em uma lista
+    # Separa os valores da entrada em uma lista e aplica o mapeamento pra cada valor da lista
     valores = entrada.split(',')
-    # Aplica o mapeamento a cada valor da lista
     valores_numeros.append(np.array([mapa[x] for x in valores]))
 
 dadosTabuleiroArr = np.array(valores_numeros)
 dadosClassificacoArr = np.array(dadosClassificaco)
 
-# Define o algoritmo k-NN com k=3
-knn = KNeighborsClassifier(n_neighbors=3, metric='euclidean')
+# Define o algoritmo k-NN com k=4
+knn = KNeighborsClassifier(n_neighbors=4, metric='euclidean')
 
 # Treina o algoritmo com as instâncias de treinamento e as classes correspondentes
 knn.fit(dadosTabuleiroArr, dadosClassificacoArr)
 
-# Define a instância a ser testada
-X_test = ['x,x,x,x,o,o,x,o,o'] #[-1, 0, 0, -1, 1, 1, -1, 1, 1]
+def verifica_acabou(tabuleiroAtual):
 
-entradaConvertida = [mapa[x] for x in X_test[0].replace(',', '')]
+    # Define a instância a ser testada
+    #X_test = ['x,x,x,x,o,o,x,o,o'] #[-1, 0, 0, -1, 1, 1, -1, 1, 1]
 
-# Classifica a instância de teste com base nas instâncias de treinamento
-prediction = knn.predict(np.array(entradaConvertida).reshape(1,-1))
+    entradaConvertida = [mapa[x] for x in tabuleiroAtual.replace(',', '')]
 
-# Imprime a classe prevista para a instância de teste
-print("Ainda tem jogo!" if prediction == "negative" else "Jogo acabou!")
+    # Classifica a instância de teste com base nas instâncias de treinamento
+    prediction = knn.predict(np.array(entradaConvertida).reshape(1,-1))
+    print("prediction: ")
+    print(prediction)
 
+    # Imprime a classe prevista para a instância de teste
+    #print("\nAinda tem jogo!\n" if prediction == "negative" else "\nJogo acabou!\n")
+    #return (True if prediction == "negative" else False)
+    return False
+
+
+
+# Definindo o tabuleiro
+tabuleiro = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+# Função para imprimir o tabuleiro
+def imprime_tabuleiro():
+    print(tabuleiro[0] + '|' + tabuleiro[1] + '|' + tabuleiro[2])
+    print('-+-+-')
+    print(tabuleiro[3] + '|' + tabuleiro[4] + '|' + tabuleiro[5])
+    print('-+-+-')
+    print(tabuleiro[6] + '|' + tabuleiro[7] + '|' + tabuleiro[8])
+
+# Função principal do jogo
+def jogo_da_velha():
+    jogando = True
+    jogador_atual = 'x'
+
+    while jogando:
+        imprime_tabuleiro()
+        jogada = input("Entre a posição desejada: ")
+        
+        # Verifica se a jogada é válida
+        while jogada not in ['1', '2', '3', '4', '5', '6', '7', '8', '9'] or tabuleiro[int(jogada)-1] != ' ':
+            jogada = input("Entre a posição desejada: ")
+
+        # Atualiza o tabuleiro com a jogada do jogador atual
+        tabuleiro[int(jogada)-1] = jogador_atual
+
+        #num_nao_vazios = len([elem for elem in tabuleiro if elem.strip() != ''])
+
+        # Verifica se houve vencedor, se nao passa a vez para o próximo jogador
+        if verifica_acabou((",".join([elem.strip() if elem.strip() != '' else 'b' for elem in tabuleiro]))):
+            imprime_tabuleiro()
+            jogando = False
+        else:
+            if jogador_atual == 'x':
+                jogador_atual = 'o'
+            else:
+                jogador_atual = 'x'
+
+# Inicia o jogo
+jogo_da_velha()
